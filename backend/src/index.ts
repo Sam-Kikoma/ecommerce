9bbbf7068e0f3f5f0651ca511daa89a480355154
Sam Kikoma <kikomasamson@gmail.com>
2025-01-22 23:20:02 +0100

    feat: server entry point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import { PORT } from "./secrets";
+import rootRouter from "./routes";
+import { PrismaClient } from "@prisma/client";
+import { errorMiddleware } from "./middleware/errors";
 
 const app: Express = express();
-
-app.get("/", (req: Request, res: Response) => {
-	res.send("Live");
+export const prismaClient = new PrismaClient({
+	log: ["query"],
 });
 
-app.listen(3000, () => {
+app.use(express.json());
+app.use("/api", rootRouter);
+app.use(errorMiddleware);
+
+app.listen(PORT, () => {
 	console.log("We are live");
 });
